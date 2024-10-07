539d2e518b38b616a3a4578a651a46d74c8c8f29
AaryaTalgaonkar <125786542+AaryaTalgaonkar@users.noreply.github.com>
2024-10-07 11:52:11 +0530

    Add files via upload

--- p3_2.py
+++ p3_2.py
@@ -1,11 +1,10 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls,CONFIG_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet, ethernet,lldp, ether_types
+from ryu.lib.packet import packet, ethernet,lldp, ether_types,arp
 from ryu.topology import event as topo_event
-from ryu.topology.api import get_host
 import time
 import logging
 class SimpleSwitch(app_manager.RyuApp):
@@ -13,8 +12,8 @@ class SimpleSwitch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.logger = logging.getLogger('my_logger')
-        self.logger.setLevel(logging.DEBUG)
-
+        self.logger.setLevel(logging.INFO)
+        logging.getLogger('ryu').setLevel(logging.WARNING)
         # Console handler
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
@@ -31,9 +30,9 @@ class SimpleSwitch(app_manager.RyuApp):
         self.adj_lists={}
 
         #stores (switch_dpid,port) to mac address mapping
-        self.hostmac_to_switch_port={}
+        self.mac_to_switch_port={}
         self.path={}
-        self.sent_timestamps={}
+        self.lldp_pkt_count={}
         
     def build_lldp_packet(self, src_dpid,src_port,src_mac):
         # Create Ethernet frame
@@ -57,47 +56,27 @@ class SimpleSwitch(app_manager.RyuApp):
         ## Code Build LLDP packet
         lldp_pkt = packet.Packet()
         lldp_pkt.add_protocol(eth)
-        ldelpkt = lldp.SystemDescription(system_description=b'SDN')
+        tm=time.time()
+        descr=f'SDN-{tm}'
+        ldelpkt = lldp.SystemDescription(system_description=descr.encode('utf-8'))
         lldp_pkt.add_protocol(lldp.lldp(tlvs=[chassis_id, port_id, ttl,ldelpkt]))
         lldp_pkt.serialize()
         return lldp_pkt
     def send_for_link_delay(self,link):
         src_dpid=link.src.dpid
         src_port=link.src.port_no
-        dst_dpid=link.dst.dpid
-        dst_port=link.dst.port_no
         src_mac=self.datapaths[src_dpid].ports[src_port].hw_addr
-        dst_mac=self.datapaths[dst_dpid].ports[dst_port].hw_addr
-        pkt=self.build_lldp_packet(src_dpid,src_port,src_mac)
         datapath=self.datapaths[src_dpid]
         actions = [datapath.ofproto_parser.OFPActionOutput(port=src_port)]
+        pkt=self.build_lldp_packet(src_dpid,src_port,src_mac)
         out = datapath.ofproto_parser.OFPPacketOut(datapath=datapath,
             buffer_id=datapath.ofproto.OFP_NO_BUFFER,
             in_port=datapath.ofproto.OFPP_CONTROLLER,
             actions=actions,
             data=pkt.data  # The serialized LLDP packet data
         )
-        self.sent_timestamps[(src_mac,dst_mac)]=time.time()
         datapath.send_msg(out)
     def set_min_links(self):
-        # undirected_link_delays={}
-        # for (sw1,sw2) in self.links:
-        #     for (p1,p2) in self.links[(sw1,sw2)]:
-        #         mac1=self.datapaths[(sw1,p1)]
-        #         mac2=self.datapaths[(sw2,p2)]
-        #         delay=0
-        #         count=0
-        #         if((mac1,mac2) in self.link_delays):
-        #             delay+=self.link_delays[(mac1,mac2)]
-        #             count+=1
-        #         if((mac2,mac1) in self.link_delays):
-        #             delay+=self.link_delays[(mac2,mac1)]
-        #             count+=1
-        #         if(count==0):
-        #             undirected_link_delays[(mac1,mac2)]=float('inf')
-        #             continue
-        #         delay=(delay/count)
-        #         undirected_link_delays[(mac1,mac2)]=delay
         for (sw1,sw2) in self.links:
             all_links=self.links[(sw1,sw2)]
             min_del_link = min(all_links, key=lambda link: self.link_delays[(self.datapaths[sw1].ports[link[0]].hw_addr,self.datapaths[sw2].ports[link[1]].hw_addr)])
@@ -150,6 +129,7 @@ class SimpleSwitch(app_manager.RyuApp):
             flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
         datapath.send_msg(mod)
     def broadcast_handler(self,ev):
+        return
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -185,17 +165,13 @@ class SimpleSwitch(app_manager.RyuApp):
         eth = pkt.get_protocol(ethernet.ethernet)
         dst_mac = eth.dst      
         start_dpid = datapath.id
-        end_dpid=self.hostmac_to_switch_port[dst_mac][0]
+        end_dpid=self.mac_to_switch_port[dst_mac][0]
         if(start_dpid==end_dpid):
-            out_port=self.hostmac_to_switch_port[dst_mac][1]
+            out_port=self.mac_to_switch_port[dst_mac][1]
         else:
             next_hop=self.path[start_dpid][end_dpid][1]
-            if((start_dpid,next_hop) in self.links):
-                min_del_link = min(self.links[start_dpid][next_hop], key=lambda link: self.link_delays[(self.dpset.get_port(start_dpid,link[0]),self.dpset.get_port(next_hop,link[1]))])
-                out_port=min_del_link[0]
-            else:
-                min_del_link = min(self.links[next_hop][start_dpid], key=lambda link: self.link_delays[(self.dpset.get_port(next_hop,link[0]),self.dpset.get_port(start_dpid,link[1]))])
-                out_port=min_del_link[1]
+            min_del_link = min(self.links[start_dpid][next_hop], key=lambda link: self.link_delays[(self.datapaths[start_dpid].ports[link[0]].hw_addr,self.datapaths[next_hop].ports[link[1]].hw_addr)])
+            out_port=min_del_link[0]
         actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
@@ -207,27 +183,37 @@ class SimpleSwitch(app_manager.RyuApp):
         datapath.send_msg(out)
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        recv_time=time.time()
+        recv_time=time.time()   
         msg = ev.msg
         pkt = packet.Packet(msg.data)
         datapath = msg.datapath
         eth = pkt.get_protocol(ethernet.ethernet)
         src_mac=eth.src
+        dst_mac=eth.dst
         in_port = msg.match['in_port']
-        dst_mac = datapath.ports[in_port].hw_addr
+        arp_pkt = pkt.get_protocol(arp.arp)
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             lldp_pkt = pkt.get_protocol(lldp.lldp)
             for tlv in lldp_pkt.tlvs:
                 if isinstance(tlv, lldp.SystemDescription):
-                    if tlv.system_description == b'SDN':
-                        send_time = self.sent_timestamps.get((src_mac,dst_mac))
-                        link_delay = recv_time-send_time  
-                        del self.sent_timestamps[(src_mac,dst_mac)]
+                    descr=tlv.system_description.decode('utf-8')
+                    if  descr.startswith('SDN'):
+                        src_mac=eth.src
+                        dst_mac = datapath.ports[in_port].hw_addr
+                        _, send_time = descr.split(" - ")
+                        link_delay = (recv_time-send_time)*1000
+                        self.lldp_pkt_count[(src_mac,dst_mac)]+=1
+                        count=self.lldp_pkt_count[(src_mac,dst_mac)]
+                        link_delay=(self.link_delays[(src_mac,dst_mac)]*(count-1)+link_delay)/count
                         self.link_delays[(src_mac,dst_mac)]=link_delay
-                        self.logger.info(link_delay)
+                        self.logger.info("Source mac : "+str(src_mac)+ " Destination mac : "+str(dst_mac)+" "+str(link_delay))
                         self.run_floyd_warshall()
             return 
-        if dst_mac=='ff:ff:ff:ff:ff:ff':
+        if dst_mac.startswith("33:33"):
+            self.broadcast_handler(ev)
+        elif dst_mac.startswith("01:00:5e"):
+            self.broadcast_handler(ev)
+        elif (dst_mac=='ff:ff:ff:ff:ff:ff'):
             self.broadcast_handler(ev)
         else:
             self.unicast_handler(ev)
@@ -239,16 +225,30 @@ class SimpleSwitch(app_manager.RyuApp):
         dpid = datapath.id
         self.logger.info("Added switch")
         self.datapaths[dpid]=datapath
-    @set_ev_cls(topo_event.EventHostAdd, MAIN_DISPATCHER)
+    def handle_arp(self, datapath, in_port, pkt):
+        arp_pkt = pkt.get_protocol(arp.arp)
+        
+        if arp_pkt.opcode == arp.ARP_REQUEST:
+            self.logger.info(f"Received ARP Request from {arp_pkt.src_ip} for {arp_pkt.dst_ip}")
+            
+            # Check if we know the destination IP (i.e., the MAC address of the destination)
+            if arp_pkt.dst_ip in self.hostmac_to_switch_port:
+                self.send_arp_reply(datapath, in_port, arp_pkt)
+            else:
+                # Flood the ARP request if we don't know the destination
+                self.flood_arp_request(datapath, in_port, pkt)
+        
+        elif arp_pkt.opcode == arp.ARP_REPLY:
+            self.logger.info(f"Received ARP Reply from {arp_pkt.src_ip} with MAC {arp_pkt.src_mac}")
+            # Process ARP replies and update internal mappings
+            self.hostmac_to_switch_port[arp_pkt.src_ip] = (datapath.id, in_port)
+            self.forward_arp_reply(datapath, in_port, pkt)
+    @set_ev_cls(topo_event.EventHostAdd, CONFIG_DISPATCHER)
     def host_add_handler(self, ev):
         self.logger.info("Host added")
-        hosts=get_host(self,None)
-        mac_addresses=[]
-        for host in hosts:
-            mac_addresses.append(host.hw_addr)
-            self.logger.info("Address : "+str(host.hw_addr))
         host = ev.host  # Get the host object from the event
-        self.hosts.add(host.hw_addr)  # Add the host to the list
+        self.logger.info(host.mac)
+        self.mac_to_switch_port[host.mac]=(host.port.dpid,host.port.port_no)
     @set_ev_cls(topo_event.EventLinkAdd, MAIN_DISPATCHER)
     def add_link(self,ev):
         link=ev.link
@@ -259,7 +259,9 @@ class SimpleSwitch(app_manager.RyuApp):
         mac1=self.datapaths[sw1].ports[p1].hw_addr
         mac2=self.datapaths[sw2].ports[p2].hw_addr
         self.link_delays[(mac1,mac2)]=float('inf')
-        self.send_for_link_delay(link)
+        self.lldp_pkt_count[(mac1,mac2)]=0
+        for _ in range(5):
+            self.send_for_link_delay(link)
         if (sw1,sw2) in self.links:
             self.links[(sw1,sw2)].append((p1,p2))
         else:
